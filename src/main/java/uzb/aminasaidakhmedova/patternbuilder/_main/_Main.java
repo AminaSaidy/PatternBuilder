26c6446b03c888d08f7dcf954764aa8cf7e98d33
Amina Saidaxmedova <aminasaidakhmedova@gmail.com>
2024-07-17 19:21:20 +0500

    in main created an arraylist of 20 empty User.Builder objects

--- src/main/java/uzb/aminasaidakhmedova/patternbuilder/_main/_Main.java
+++ src/main/java/uzb/aminasaidakhmedova/patternbuilder/_main/_Main.java
@@ -1,13 +1,15 @@
 package uzb.aminasaidakhmedova.patternbuilder._main;
 
+import uzb.aminasaidakhmedova.patternbuilder.model.entity.User;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class _Main {
     public static void main(String[] args) {
-        List<String> userBuild = new ArrayList<>();
+        List<User.Builder> userBuild = new ArrayList<>();
         for (int i = 0; i < 20; i++) {
-            userBuild.add("");
+            userBuild.add(new User.Builder());
         }
     }
 }
