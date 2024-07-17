ada20bbe1a6fcba830fbe5750bfdc4f59c97e2fe
Amina Saidaxmedova <aminasaidakhmedova@gmail.com>
2024-07-17 19:14:17 +0500

    added constructor and getters to class User

--- src/main/java/uzb/aminasaidakhmedova/patternbuilder/model/entity/User.java
+++ src/main/java/uzb/aminasaidakhmedova/patternbuilder/model/entity/User.java
@@ -1,8 +1,31 @@
 package uzb.aminasaidakhmedova.patternbuilder.model.entity;
 
 public class User {
-    Long id;
-    String login;
-    int age;
-    StringBuilder password;
+    private final Long id;
+    private final String login;
+    private final int age;
+    private final StringBuilder password;
+
+    public User(Long id, String login, int age, StringBuilder password) {
+        this.id = id;
+        this.login = login;
+        this.age = age;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public StringBuilder getPassword() {
+        return password;
+    }
 }
